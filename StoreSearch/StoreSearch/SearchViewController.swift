90fa1e5d293189a55efac0679aacef0192d4839c
Unknown <campbell.graham@gridstone.com.au>
2018-02-20 10:34:54 +1100

    Layout for main view controller in place with table view and searchbar

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Campbell Graham on 20/2/18.
+//  Copyright © 2018 someIndustry. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let searchBar = UISearchBar()
+    let tableView = UITableView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //title and colours
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        title = "Search"
+      
+        //add items to view
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        
+        //visual constraints
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            //search bar
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            //tableview
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = String(indexPath.row)
+        return cell!
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

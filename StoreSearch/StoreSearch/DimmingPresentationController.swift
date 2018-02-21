aad127922f4432d28b96a9e06aa5b964e7a9e6fc
Unknown <campbell.graham@gridstone.com.au>
2018-02-22 10:18:19 +1100

    Gradient now in background of detail view controller

--- StoreSearch/StoreSearch/DimmingPresentationController.swift
+++ StoreSearch/StoreSearch/DimmingPresentationController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class DimmingPresentationController: UIPresentationController {
+    
+    lazy var dimmingView = GradientView(frame: CGRect.zero)
+    
     override var shouldRemovePresentersView: Bool {
         return false
+    }
+    
+    override func presentationTransitionWillBegin() {
+        dimmingView.frame = containerView!.bounds
+        containerView?.insertSubview(dimmingView, at: 0)
     }
 }

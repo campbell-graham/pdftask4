20bba4ebda6c70ff3ae991e7a6bc2700c0f7e5bd
Unknown <campbell.graham@gridstone.com.au>
2018-02-22 12:16:59 +1100

    Blank scroll view and page control are now in landscape view controller

--- StoreSearch/StoreSearch/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
+    
+    let scrollView = UIScrollView()
+    let pageControl = UIPageControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        pageControl.numberOfPages = 3
+        
+        //add items to view
+        view.addSubview(scrollView)
+        view.addSubview(pageControl)
+        
+        //layout constraints
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+                //page control
+                pageControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                pageControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+                //scroll view
+                scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
 
         // Do any additional setup after loading the view.
     }
